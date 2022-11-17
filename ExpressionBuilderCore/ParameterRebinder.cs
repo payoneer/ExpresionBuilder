60a9520a08ba28d42d86b7371f40c9df53ee2e72
michael berezin <mbearz@gmail.com>
2022-11-17 19:37:35 +0200

    renamed to   ParameterRebinder

--- ExpressionBuilderCore/ParameterRebinder.cs
+++ ExpressionBuilderCore/ParameterRebinder.cs
@@ -5,20 +5,19 @@ namespace ExpressionBuilderCore
 {
     //for more info read the blog posts:
     //https://learn.microsoft.com/en-us/archive/blogs/meek/linq-to-entities-combining-predicates
-    //https://learn.microsoft.com/en-us/archive/blogs/mattwar/linq-building-an-iqueryable-provider-part-ii
 
-    public class ParameterReplacer : ExpressionVisitor
+    public class ParameterRebinder : ExpressionVisitor
     {
         private readonly Dictionary<ParameterExpression, ParameterExpression> map;
 
-        public ParameterReplacer(Dictionary<ParameterExpression, ParameterExpression> map)
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
         {
             this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
         }
 
         public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
         {
-            return new ParameterReplacer(map).Visit(exp);
+            return new ParameterRebinder(map).Visit(exp);
         }
 
         protected override Expression VisitParameter(ParameterExpression p)
